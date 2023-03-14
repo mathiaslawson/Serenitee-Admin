c0d8182621f8a3a11befadedeab78a1b641fd348
ingucell <mathiaslawson70@gmail.com>
2023-03-14 17:18:17 +0000

    mui theme re addition

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {Provider} from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import store from './store';
 import App from './containers/App';
+import { ColorModeContext, useMode } from './theme';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-   <Provider store={store}>
-    <Router>
-         <App />
-      </Router>
-   </Provider>
-  </React.StrictMode>
-);
 
+function Index() {
+  const [theme, colorMode] = useMode()
 
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <ColorModeContext.Provider value={colorMode}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+          <div className='app'>
+            <main className='content'>
+          <Router>
+            <App />
+          </Router>
+          </main>
+          </div>
+          </ThemeProvider>
+        </ColorModeContext.Provider>
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
