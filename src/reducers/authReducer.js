68e50ba32481fc83955667e442d8442380c9561e
ingucell <mathiaslawson70@gmail.com>
2023-03-14 16:05:12 +0000

    load and auth reducer

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,16 @@
+import {SIGN_IN, SIGN_OUT} from '../constants/actionTypes'
+
+const INITIAL_STATE = {
+    user: null
+}
+
+const authReducer = (state = INITIAL_STATE, action) =>{
+    switch (action.type){
+        case SIGN_IN: 
+        return {...state, user: action.payload};
+        default: 
+        return state;
+    }
+}
+
+export default authReducer
