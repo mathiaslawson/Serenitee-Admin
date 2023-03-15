dab8acedec0b4987672d2e9b44167a77b967847d
ingucell <mathiaslawson70@gmail.com>
2023-03-15 14:46:46 +0000

    authStateObserver for state persistence

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,12 +1,38 @@
-import React, { Fragment } from 'react';
-import Routes from '../../routes'
+import React, { Component, Fragment } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { SignIn } from '../../actions';
+import { withFirebase } from '../../services/index';
+import { authStateObserver } from '../../utils/authStateObserver';
 
-const App = () => {
-  return (
-    <Fragment>
-      <Routes />
-    </Fragment>
-  );
+import Routes from '../../routes';
+
+
+class App extends Component {
+  componentDidMount() {
+    const { firebase, SignIn } = this.props;
+    authStateObserver(firebase, SignIn);
+  }
+
+  render() {
+    console.log(this.props.user);
+
+    return (
+      <Fragment>
+        <Routes />
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { user: state.auth.user };
 };
 
-export default App;
+export default compose(
+  connect(
+    mapStateToProps,
+    { SignIn }
+  ),
+  withFirebase
+)(App);
